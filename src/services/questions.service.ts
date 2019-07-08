fe5955e5598fffcf1434bc1b115dc7337dd22636
Tanvi Jivtode <tanvi.jivtode@davita.com>
2019-07-08 10:16:39 -0500

    Fully functioning, yet to be refactored

--- src/services/questions.service.ts
+++ src/services/questions.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Question } from 'src/app/objects/question';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+//import questions from  '../assets/mockData/questions.json';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +10,15 @@ import { Observable } from 'rxjs';
 export class QuestionsService {
 
   constructor(protected http: HttpClient) { 
+   
     this.getQuestions().subscribe(data => {
                   console.log(data);
               });
+
   }
 
   public getQuestions(): Observable<Question[]> {
-  
+  //return of <Question[]>(questions);
     return this.http.get<Question[]>('./assets/mockData/questions.json');
   }
 }
